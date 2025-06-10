a04718069d10e0e9105405eacc9d8765faae1812
Nirmit Kaundal <nirmit.kaundal@nirmitss-451.successivetech.com>
2025-06-10 07:01:07 -0400

    logic improved

--- assignment-6/Q4.js
+++ assignment-6/Q4.js
@@ -15,5 +15,24 @@ class Person{
 
     fullName(){
         const result = `My fullname is ${this.firstName} ${this.lastName} and I am ${this.age} years old`
+        return result;
+    }
+
+    averageAge(arr){
+        const sum = arr.reduce((acc,i)=>acc+i.age,0);
+        return sum/arr.length;
     }
 }
+
+const person1 = new Person('john','wick',10);
+const person2 = new Person('sam','smith',10);
+const person3 = new Person('raj','kumar',10);
+const person4 = new Person('sally','wally',10);
+
+const avgSum = person1.averageAge([person1,person2,person3,person4]);
+console.log(avgSum);
+
+const ans= person1.fullName()
+console.log(ans);
+
+
