558795422940596e0af49b12a2930365c93c98ec
Nirmit Kaundal <nirmit.kaundal@nirmitss-451.successivetech.com>
2025-06-12 07:11:41 -0400

    logic improved

--- assignment-7/Q8.js
+++ assignment-7/Q8.js
@@ -1,28 +1,46 @@
 // 8. Create a function that fetches data from multiple APIs in parallel and then performs some operation on the combined data, using async/await.
 
-const child1 =()=>{
-    let r=fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then((response)=>response.json());
+const fetchMultipleData=()=>{
+    const promise1 = new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then((response)=>response.json())
+            .then((data)=>resolve(data))
+            .catch((error)=>reject(error))
+        }, 3000);
+    })
 
-    return r;
+        const promise2 = new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/todos/2')
+            .then((response)=>response.json())
+            .then((data)=>resolve(data))
+            .catch((error)=>reject(error))
+        }, 3000);
+    })
+
+        const promise3 = new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/todos/3')
+            .then((response)=>response.json())
+            .then((data)=>resolve(data))
+            .catch((error)=>reject(error))
+        }, 3000);
+    })
+
+    return [promise1,promise2,promise3];
+    
 }
 
-const child2 =()=>{
-    let p=fetch('https://jsonplaceholder.typicode.com/todos/2')
-    .then((response)=>response.json());
-
-    return p;
-}
 
 const headFunction=async()=>{
-    let result1 =await child1();
-    let result2 =await child2();
-    console.log(result1.id);
-    console.log(result2.id);
-
-    console.log(result1.id + result2.id)
-
-    // let result3 = await result1[1] + result2[1];
+    try {
+        const result =  fetchMultipleData();
+        const ans = await Promise.allSettled(result)
+        console.log(`The sum of id from three api are:${ans[0].value.id + ans[1].value.id + ans[2].value.id}`);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
